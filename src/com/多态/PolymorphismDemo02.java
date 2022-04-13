4116db6fa514f5364e54d18334b7c2079168d0bc
MrKangM <736627699@qq.com>
2022-04-13 23:30:50 +0800

    多态代码

--- src/com/多态/PolymorphismDemo02.java
+++ src/com/多态/PolymorphismDemo02.java
@@ -6,7 +6,7 @@ package com.多态;
 *       静态方法：编译时看左边，运行时输出左边
 *
 *   总结：
-*       编译时都看左边，只有运行成员方法时输出右边
+*       编译时都看左边，只有运行成员方法时输出右边。
 * */
 public class PolymorphismDemo02 {
     public static void main(String[] args) {
