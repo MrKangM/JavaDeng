8850c5daf6d1dd5a09821599352796f9c54d2ea1
MrKangM <736627699@qq.com>
2022-04-16 20:49:37 +0800

    第一次提交

---
+++ src/com/duotai/PolymorphismDemo01.java
@@ -0,0 +1,32 @@
+package com.duotai;
+/*
+* 多态的实现：
+*   字父类继承关系
+*   方法重写
+*   父类引用指向子类子类对象
+* */
+public class PolymorphismDemo01 {
+    public static void main(String[] args) {
+        cat c = new cat();
+        c.eat();
+//      父类引用指向子类对象
+//      父类：Animal类
+//      子类：cat类
+        Animal a = new cat();
+        a.eat();
+    }
+}
+
+class Animal{
+    public void eat(){
+        System.out.println("吃东西");
+    }
+}
+
+//字父类继承
+class cat extends Animal{
+//    方法重写
+    public void eat(){
+        System.out.println("猫吃鱼");
+    }
+}
